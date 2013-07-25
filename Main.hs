2c146546217c3f3070d111c3a46b1fc4a8ce091d
Simon <simon.parry@coshx.co.uk>
2013-07-25 07:52:10 +0000

    testing commit from fpcomplete

--- Main.hs
+++ Main.hs
@@ -2,4 +2,4 @@ module Main (main) where
 
 import SimpleJSON
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = print (JObject [("foo", JNumber 1), ("bar", JBool False), ("baz", JString "hello")])
